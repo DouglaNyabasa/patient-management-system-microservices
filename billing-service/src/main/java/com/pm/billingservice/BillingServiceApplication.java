e667638c8bf82d1b03806e171245044fbd2815b3
douglanyabasa <douglasnyabasa400@gmail.com>
2025-04-07 16:50:30 +0200

    dependancy resolved

--- billing-service/src/main/java/com/pm/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/pm/billingservice/BillingServiceApplication.java
@@ -1,10 +1,11 @@
 package com.pm.billingservice;
 
+import billing.BillingServiceGrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BillingServiceApplication {
+public class BillingServiceApplication extends BillingServiceGrpc.BillingServiceImplBase {
 
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
