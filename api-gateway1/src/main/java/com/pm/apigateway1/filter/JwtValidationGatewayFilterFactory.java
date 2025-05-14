e9e816a0c07dd72bb924cc42a87e212a0a820bc9
douglanyabasa <douglasnyabasa400@gmail.com>
2025-05-14 12:48:44 +0200

    api gateway config

--- api-gateway1/src/main/java/com/pm/apigateway1/filter/JwtValidationGatewayFilterFactory.java
+++ api-gateway1/src/main/java/com/pm/apigateway1/filter/JwtValidationGatewayFilterFactory.java
@@ -4,6 +4,8 @@ package com.pm.apigateway1.filter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,8 +20,18 @@ public class JwtValidationGatewayFilterFactory extends AbstractGatewayFilterFact
     @Override
     public GatewayFilter apply(Object config) {
        return (exchange, chain) -> {
-
-       }
+          String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+          if (token == null || !token.startsWith("Bearer ")) {
+              exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+              return exchange.getResponse().setComplete();
+          }
+          return webClient.get()
+                  .uri("/validate")
+                  .header(HttpHeaders.AUTHORIZATION, token)
+                  .retrieve()
+                  .toBodilessEntity()
+                  .then(chain.filter(exchange));
+       };
     }
 
 
